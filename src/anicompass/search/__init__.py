65a19f3a5bcbac6cd994321c9078e1e879a41817
一绵烊 <17323765341@163.com>
2026-08-05 09:02:16 +0800

    Add src/anicompass/search/__init__.py

---
+++ src/anicompass/search/__init__.py
@@ -0,0 +1,6 @@
+"""Search view-model and Qt bridge package."""
+
+from anicompass.search.bridge import SearchBridge
+from anicompass.search.viewmodel import SearchStatus, SearchViewModel, SearchViewState
+
+__all__ = ["SearchBridge", "SearchStatus", "SearchViewModel", "SearchViewState"]
